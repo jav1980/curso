74bb62f1218619390e16bcd30cb30e6459e2036f
jav1980 <156695290+jav1980@users.noreply.github.com>
2025-01-27 14:40:07 -0600

    Update linear.py

--- linear.py
+++ linear.py
@@ -36,11 +36,11 @@ linear_model.compile(optimizer=tf.keras.optimizers.SGD(), loss=tf.keras.losses.m
 print(linear_model.summary())
 w,b = linear_model.weights
 print('before..')
-print(w.numpy)
+print(w.numpy())
 print(b)
 linear_model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=20)
 print('..after')
-print(w.numpy)
+print(w.numpy())
 print(b)
 
 #print(linear_model.predict([ [0.0], [2.0], [3.1], [4.2], [5.2] ] ).tolist() )   
