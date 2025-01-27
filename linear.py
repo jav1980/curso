a30797c54c2837f6fa702e309bfd9894c95b404e
jav1980 <156695290+jav1980@users.noreply.github.com>
2025-01-27 14:02:40 -0600

    Update linear.py

--- linear.py
+++ linear.py
@@ -12,7 +12,7 @@ print(tf.__version__)
 X = np.arange(-10.0, 10.0, 1e-2)
 print(X)
 #np.random.shuffle(X)
-y =  10.0 * X + 0.0
+y =  2.0 * X + 1.0
 print(y)
 train_end = int(0.6 * len(X))
 #print (train_end)
